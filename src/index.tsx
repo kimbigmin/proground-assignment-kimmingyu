9b9645967b944635029961f7a2a0b47e9580263b
kimmingyu <76110448+kimbigmin@users.noreply.github.com>
2022-08-13 05:27:43 +0900

    [Add]: ScrollToTop 컴포넌트 index.tsx에 적용

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 let persistor = persistStore(store);
 
@@ -15,7 +17,10 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <App />
+        <Router>
+          <ScrollToTop />
+          <App />
+        </Router>
       </PersistGate>
     </Provider>
   </React.StrictMode>
